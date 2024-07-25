1c332f422dbd00d269fccb2cce17bde347183cda
reuven <r0583247229@gmail.com>
2024-07-25 17:26:45 +0300

    Add project files.

---
+++ WebApplication1/Controllers/CarController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Model(string car, int year)
+        {
+            Car car1 = new Car { car = car, year = year };
+
+            return View(car1);
+        }
+
+    }
+}
---
+++ WebApplication1/Models/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class Car
+    {
+        public string car {  get; set; }
+        public int year { get; set; }
+
+    }
+}
